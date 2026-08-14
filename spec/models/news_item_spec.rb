4fdcd3133585281acc7488a4b1f0cc36ce3bcb87
Leeahnie Anderson <l237766a@berkeley.edu>
2026-08-14 13:16:09 -0700

    added issues to news article and test coverage

--- spec/models/news_item_spec.rb
+++ spec/models/news_item_spec.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: news_items
+#
+#  id                :integer          not null, primary key
+#  description       :text
+#  issue             :string
+#  link              :string           not null
+#  title             :string           not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  representative_id :integer          not null
+#
+# Indexes
+#
+#  index_news_items_on_representative_id  (representative_id)
+#
 require 'rails_helper'
 
 RSpec.describe NewsItem do
@@ -20,9 +37,39 @@ RSpec.describe NewsItem do
         description: 'Test description'
       )
     end
-
     it 'finds a news item for the representative' do
       expect(described_class.find_for(representative.id)).to eq(news_item)
+    end
+  end
+    describe '.issues' do
+    it 'returns the required list of issues' do
+      expect(described_class.issues).to include(
+        'Free Speech',
+        'Immigration',
+        'Climate Change',
+        'Gun Control',
+        'Equal Pay'
+      )
+    end
+  end
+
+  describe 'issue attribute' do
+    let(:representative) do
+      Representative.create!(
+        name: 'Jane Doe',
+        ocdid: 'ocd-456',
+        title: 'Representative'
+      )
+    end
+    it 'stores an issue for a news item' do
+      news_item = described_class.create!(
+        representative: representative,
+        title: 'Climate Article',
+        link: 'https://example.com/climate',
+        description: 'An article about climate change',
+        issue: 'Climate Change'
+      )
+      expect(news_item.reload.issue).to eq('Climate Change')
     end
   end
 end
